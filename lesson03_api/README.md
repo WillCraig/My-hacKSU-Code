5c14a476f9e78221c20c85e90a085bf976cd9e7f
William Craig <wdcraig99@gmail.com>
2022-09-14 09:22:56 -0400

    formatting and updates
    Readme updates

--- lesson03_api/README.md
+++ lesson03_api/README.md
@@ -1,1 +1,6 @@
 # Lesson 3
+Intructor: Tony
+
+Went through the basics of accessing and using an API within python.
+
+API used was OpenWeather TODO: add link
--- lesson03_api/lesson_code.py
+++ lesson03_api/lesson_code.py
@@ -5,54 +5,38 @@ import json
 KEY = ""
 
 
-
 def get_weather(zip_code):
-    url = 'http://api.openweathermap.org/data/2.5/weather?zip={}&appid={}'.format(zip_code, KEY) #
-    
+    url = 'http://api.openweathermap.org/data/2.5/weather?zip={}&appid={}'.format(
+        zip_code, KEY)
+
     response = requests.get(url)
-    
+
     data = response.json()
-    
+
     pretty_data = json.dumps(data, indent=4)
-    
+
     city = data['name']
-    
+
     print(city)
-    
-    
-    
-    
+
     tempK = data['main']['temp']
-    
+
     tempF = 1.8 * (tempK - 273) + 32
-    
-    
+
     print(tempK)
     print(tempF)
-    print(int(tempF)) # shortcut to getting no floating output, would rather use output formatting 
-    
-    
-    print("\n\n\n\n") # separator
-    
-    
-    print(f"The temperature in {city} is {tempF:.2f}") 
+    # shortcut to getting no floating output, would rather use output formatting
+    print(int(tempF))
 
-    
-    
+    print("\n\n\n\n")  # separator
+
+    print(f"The temperature in {city} is {tempF:.2f}")
+
     # print(pretty_data)
-    
-
-
-
 
 
 if __name__ == '__main__':
-    
+
     zip = 44262
-    
+
     get_weather(zip)
-    
-    
-    
-    
-    
